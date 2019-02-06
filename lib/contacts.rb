a6d2ba53ca448c0a095cdca29bfee93391a65bf8
David Acosta <davidacosta25081@yahoo.com>
2019-02-06 07:03:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,9 +14,10 @@ require 'pry'
      }
    }
 
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-  contact_details_hash.each do |attribute, data|
-  binding.pry
-end
-end
+   contacts.each do |person, contact_details_hash|
+     contact_details_hash.each do |attribute, data|
+       if attribute == :favorite_ice_cream_flavors
+         binding.pry
+       end
+     end
+   end
